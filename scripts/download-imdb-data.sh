8ce698105aa2b185bf0dc1c7bbd3b2511fa4c0bf
brendanmcc02 <brendan.mccann02@gmail.com>
2024-03-07 19:04:28 +0000

    copied & changed my-film-data.json

--- scripts/download-imdb-data.sh
+++
@@ -1,8 +0,0 @@
-# download the .gz files
-cd ../src/ || exit
-python3 download-imdb-gz-files.py
-
-# unzip the .gz files
-cd ../data || exit
-gzip -d title.basics.tsv.gz
-gzip -d title.ratings.tsv.gz
