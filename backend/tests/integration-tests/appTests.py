9b0d52a41ec04906e7ae5f85e4e91d6c004b1bdc
brendanmcc02 <brendan.mccann02@gmail.com>
2025-03-03 16:57:21 +0000

    wip: app int tests

--- backend/tests/integration-tests/appTests.py
+++ backend/tests/integration-tests/appTests.py
@@ -1,12 +1,15 @@
 import os
 import requests
-
+import sys
+absolutePath = os.path.dirname(os.path.abspath(__file__))
+parentDirectoryOfAbsolutePath = os.path.dirname(os.path.dirname(absolutePath))
+sys.path.append(parentDirectoryOfAbsolutePath)
+import app
 backendUrl = "http://127.0.0.1:5000"
 
 def test_verifyUserUploadedFile_imdbCorrect():
     file = open("test-csv-files/imdb-correct.csv")
     filesToSend = {'file': ("imdb-correct.csv", file)}
-
     response = requests.post(backendUrl + "/verifyUserUploadedFile", files=filesToSend)
 
     assert response.status_code == 200
@@ -16,7 +19,6 @@ def test_verifyUserUploadedFile_imdbCorrect():
 def test_verifyUserUploadedFile_imdbIncorrectHeader():
     file = open("test-csv-files/imdb-incorrect-header.csv")
     filesToSend = {'file': ("imdb-incorrect-header.csv", file)}
-
     response = requests.post(backendUrl + "/verifyUserUploadedFile", files=filesToSend)
 
     assert response.status_code == 400
@@ -26,7 +28,6 @@ def test_verifyUserUploadedFile_imdbIncorrectHeader():
 def test_verifyUserUploadedFile_imdbMissingHeader():
     file = open("test-csv-files/imdb-missing-header.csv")
     filesToSend = {'file': ("imdb-missing-header.csv", file)}
-
     response = requests.post(backendUrl + "/verifyUserUploadedFile", files=filesToSend)
 
     assert response.status_code == 400
@@ -36,7 +37,6 @@ def test_verifyUserUploadedFile_imdbMissingHeader():
 def test_verifyUserUploadedFile_letterboxdCorrect():
     file = open("test-csv-files/letterboxd-correct.csv")
     filesToSend = {'file': ("letterboxd-correct.csv", file)}
-
     response = requests.post(backendUrl + "/verifyUserUploadedFile", files=filesToSend)
 
     assert response.status_code == 200
@@ -46,7 +46,6 @@ def test_verifyUserUploadedFile_letterboxdCorrect():
 def test_verifyUserUploadedFile_letterboxdIncorrectHeader():
     file = open("test-csv-files/letterboxd-incorrect-header.csv")
     filesToSend = {'file': ("letterboxd-incorrect-header.csv", file)}
-
     response = requests.post(backendUrl + "/verifyUserUploadedFile", files=filesToSend)
 
     assert response.status_code == 400
@@ -56,9 +55,44 @@ def test_verifyUserUploadedFile_letterboxdIncorrectHeader():
 def test_verifyUserUploadedFile_letterboxdMissingHeader():
     file = open("test-csv-files/letterboxd-missing-header.csv")
     filesToSend = {'file': ("letterboxd-missing-header.csv", file)}
-
     response = requests.post(backendUrl + "/verifyUserUploadedFile", files=filesToSend)
 
     assert response.status_code == 400
     assert os.path.exists("../../../database/letterboxd-missing-header.csv")
     os.remove("../../../database/letterboxd-missing-header.csv")
+
+def test_initRowsOfRecommendations_imdbNoRecentFilms():
+    file = open("test-csv-files/imdb-no-recent.csv")
+    filesToSend = {'file': ("imdb-correct.csv", file)}
+    postResponse = requests.post(backendUrl + "/verifyUserUploadedFile", files=filesToSend)
+
+    assert postResponse.status_code == 200
+    assert os.path.exists("../../../database/imdb-correct.csv")
+
+    getResponse = requests.get(backendUrl + "/initRowsOfRecommendations")
+    assert getResponse.status_code == 200
+
+    rowsOfRecommendations = getResponse.json()
+
+    numberOfFavouriteRows = 1
+    numberOfInternationalRows = 1
+    numberOfOldRows = 1
+    total = numberOfFavouriteRows + app.NUMBER_OF_TOP_GENRE_PROFILES + numberOfInternationalRows + numberOfOldRows
+    assert len(rowsOfRecommendations) == total
+
+    for row in rowsOfRecommendations:
+        assert 'recommendedRowText' in row
+        assert row['recommendedRowsText'] != ""
+        
+        assert 'profileId' in row
+        assert row['profileId'] != ""
+
+        assert 'recommendedFilms' in row
+        assert len(row['recommendedFilms']) == app.NUMBER_OF_RECOMMENDATIONS_PER_ROW
+
+        for film in row['recommendedFilms']:
+            assert 'similarityScore' in film
+            assert film['similarityScore'] != None
+            assert film['similarityScore'] >= 0.0
+            assert film['similarityScore'] <= 100.0
+
