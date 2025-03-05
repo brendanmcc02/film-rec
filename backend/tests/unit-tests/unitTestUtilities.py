8923adb93d1c48e1896c9dc9af88f951cc146a59
brendanmcc02 <brendan.mccann02@gmail.com>
2025-03-05 16:06:42 +0000

    moved unit tests

---
+++ backend/tests/unit-tests/unitTestUtilities.py
@@ -0,0 +1,61 @@
+import os
+import sys
+# import the needed file from backend directory
+# (this is ugly as hell, there's probably an easier way but it gets the job done)
+absolutePathOfCurrentFile = os.path.dirname(os.path.abspath(__file__))
+parentDirectoryofParentDirectoryOfCurrentFile = os.path.dirname(os.path.dirname(absolutePathOfCurrentFile))
+sys.path.append(parentDirectoryofParentDirectoryOfCurrentFile)
+import initAllFilmData
+
+cacheFileLocation = "../../../database/cache.json"
+
+def verifyFilm(film, filmId, allGenres, allCountries):
+    assert 'title' in film
+    assert film['title'] != ""
+
+    assert 'letterboxdTitle' in film
+    assert film['letterboxdTitle'] != ""
+
+    assert 'year' in film
+    assert film['year'] != None
+    assert film['year'] != 0
+
+    assert 'letterboxdYear' in film
+    assert film['letterboxdYear'] != None
+    assert film['letterboxdYear'] != 0
+
+    assert 'imdbRating' in film
+    assert film['imdbRating'] != None
+    assert film['imdbRating'] >= 1.0
+    assert film['imdbRating'] <= 10.0
+
+    assert 'numberOfVotes' in film
+    assert film['numberOfVotes'] != None
+    assert film['numberOfVotes'] >= initAllFilmData.NUMBER_OF_VOTES_THRESHOLD
+
+    assert 'runtime' in film
+    assert film['runtime'] != None
+    assert film['runtime'] >= initAllFilmData.RUNTIME_THRESHOLD
+
+    assert 'runtimeHoursMinutes' in film
+    assert film['runtime'] != ""
+
+    assert 'genres' in film
+    assert len(film['genres']) > 0
+
+    for genre in film['genres']:
+        assert genre in allGenres
+
+    assert 'imdbUrl' in film
+    assert film['imdbUrl'] == initAllFilmData.BASE_IMDB_URL + filmId
+
+    assert 'countries' in film
+
+    for country in film['countries']:
+        assert country in allCountries
+
+    assert 'poster' in film
+    assert film['poster'] != ""
+
+    assert 'summary' in film
+    assert film['summary'] != ""
---
+++ backend/tests/unit-tests/vectorizeUtilities-unitTests.py
@@ -0,0 +1,111 @@
+import json
+import os
+import sys
+import unitTestUtilities
+# import the needed file from backend directory
+# (this is ugly as hell, there's probably an easier way but it gets the job done)
+absolutePathOfCurrentFile = os.path.dirname(os.path.abspath(__file__))
+parentDirectoryofParentDirectoryOfCurrentFile = os.path.dirname(os.path.dirname(absolutePathOfCurrentFile))
+sys.path.append(parentDirectoryofParentDirectoryOfCurrentFile)
+import vectorizeUtilities
+
+def test_keepVectorBoundary():
+    vector = [1.1, -0.1, 0.0]
+    vectorizeUtilities.keepVectorBoundary(vector)
+
+    for dimension in vector:
+        assert dimension >= 0.0
+        assert dimension <= 1.0
+
+    vector = [0.9, 0.1, 0.0]
+    vectorizeUtilities.keepVectorBoundary(vector)
+
+    for dimension in vector:
+        assert dimension >= 0.0
+        assert dimension <= 1.0
+
+def test_getFilmGenres():
+    cacheFile = open(unitTestUtilities.cacheFileLocation)
+    cache = json.load(cacheFile)
+    filmVectorWithActionComedyRomance = [0.0, 0.0, 0.0, 0.0, 0.7, 0.0, 0.0, 0.0, 0.7, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.7, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    assert (vectorizeUtilities.getFilmGenres(filmVectorWithActionComedyRomance, cache['allGenres']) == 
+            ["Action", "Comedy", "Romance"])
+
+    filmVectorWithAdventureDramaWestern = [0.0, 0.0, 0.0, 0.0, 0.0, 0.7, 0.0, 0.0, 0.0, 0.0, 0.0, 0.7, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.7, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    assert (vectorizeUtilities.getFilmGenres(filmVectorWithAdventureDramaWestern, cache['allGenres']) == 
+            ["Adventure", "Drama", "Western"])
+
+    filmVectorWithDrama = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.7, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    assert (vectorizeUtilities.getFilmGenres(filmVectorWithDrama, cache['allGenres']) == 
+            ["Drama"])
+
+def test_curveAccordingToMax_ZeroVector():
+    allZeroTestVector = [0.0, 0.0, 0.0]
+    testWeight = 0.5
+    testStartIndex = 0
+    vectorizeUtilities.curveAccordingToMax(allZeroTestVector, allZeroTestVector, testWeight, testStartIndex)
+    assert allZeroTestVector == [0.0, 0.0, 0.0]
+
+    allZeroTestVector = [0.0, 0.0, 0.0]
+    testWeight = 1.0
+    testStartIndex = 0
+    vectorizeUtilities.curveAccordingToMax(allZeroTestVector, allZeroTestVector, testWeight, testStartIndex)
+    assert allZeroTestVector == [0.0, 0.0, 0.0]
+
+def test_curveAccordingToMax_SingleOneVector():
+    containsSingleOneTestVector = [1.0, 0.0, 0.0]
+    testWeight = 1.0
+    testStartIndex = 0
+    vectorizeUtilities.curveAccordingToMax(containsSingleOneTestVector, containsSingleOneTestVector, 
+                                           testWeight, testStartIndex)
+    assert containsSingleOneTestVector == [1.0, 0.0, 0.0]
+
+    containsSingleOneTestVector = [1.0, 0.0, 0.0]
+    testWeight = 0.5
+    testStartIndex = 0
+    vectorizeUtilities.curveAccordingToMax(containsSingleOneTestVector, containsSingleOneTestVector, 
+                                           testWeight, testStartIndex)
+    assert containsSingleOneTestVector == [0.5, 0.0, 0.0]
+
+def test_curveAccordingToMax_multiVariableVector():
+    multiVariableTestVector = [1.0, 0.5, 0.0]
+    testWeight = 0.5
+    testStartIndex = 0
+    vectorizeUtilities.curveAccordingToMax(multiVariableTestVector, multiVariableTestVector, 
+                                           testWeight, testStartIndex)
+    assert multiVariableTestVector == [0.5, 0.25, 0.0]
+
+    multiVariableTestVector = [0.8, 0.4, 0.0]
+    testWeight = 0.5
+    testStartIndex = 0
+    vectorizeUtilities.curveAccordingToMax(multiVariableTestVector, multiVariableTestVector, 
+                                           testWeight, testStartIndex)
+    assert multiVariableTestVector == [0.5, 0.25, 0.0]
+
+    multiVariableTestVector = [0.8, 0.4, 0.0]
+    testWeight = 1.0
+    testStartIndex = 0
+    vectorizeUtilities.curveAccordingToMax(multiVariableTestVector, multiVariableTestVector, 
+                                           testWeight, testStartIndex)
+    assert multiVariableTestVector == [1.0, 0.5, 0.0]
+
+def test_getProfileMaxCountry_filmHasOnlyOneMaxCountry():
+    cacheFile = open(unitTestUtilities.cacheFileLocation)
+    cache = json.load(cacheFile)
+    allGenresLength = len(cache['allGenres'])
+
+    filmVectorWithAmerican = [0.0, 0.0, 0.0, 0.0, 0.7, 0.0, 0.0, 0.0, 0.0, 0.7, 0.0, 0.7, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+    assert (vectorizeUtilities.getProfileMaxCountry(filmVectorWithAmerican, allGenresLength, cache['allCountries']) 
+            == "American")
+
+def test_getProfileMaxCountry_filmHasMultipleMaxCountries_EnsuresOnlyOneIsReturned():
+    cacheFile = open(unitTestUtilities.cacheFileLocation)
+    cache = json.load(cacheFile)
+    allGenresLength = len(cache['allGenres'])
+
+    filmVectorWithAmericanAndBritish = [0.0, 0.0, 0.0, 0.0, 0.0, 0.7, 0.0, 0.0, 0.0, 0.0, 0.0, 0.7, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.7, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+    assert (vectorizeUtilities.getProfileMaxCountry(filmVectorWithAmericanAndBritish, allGenresLength, cache['allCountries']) 
+            == "American")
+
