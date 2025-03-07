43c35e7d16c76cbf4b828431d7aac1ddc2110785
brendanmcc02 <brendan.mccann02@gmail.com>
2025-03-07 11:25:54 +0000

    temp code

--- backend/Temp.py
+++ backend/Temp.py
@@ -1,4 +1,10 @@
-class Temp:
+# class Temp:
     
-    def __init__(self):
-        self.var = 0
+#     def __init__(self):
+#         self.var = 0
+
+from DocumentDatabase import *
+
+documentDatabase = DocumentDatabase()
+
+print(str(documentDatabase.get("allGenres")))
