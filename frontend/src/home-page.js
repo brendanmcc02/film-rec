a5f6ed44f1550c469fd378cd8ffe650f14636d4a
brendanmcc02 <brendan.mccann02@gmail.com>
2025-04-11 21:38:46 +0100

    removing redundant comment

--- frontend/src/home-page.js
+++ frontend/src/home-page.js
@@ -14,7 +14,7 @@ const App = () => {
   const handleFileSelect = (event) => {
     setSelectedFile(event.target.files[0]);
     setFileName("Selected File: " + event.target.files[0].name);
-    setErrorText(""); // Reset error text when a new file is selected
+    setErrorText("");
   };
 
   const handleFileUpload = async () => {
